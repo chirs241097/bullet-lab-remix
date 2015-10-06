16b769db4b4312b8d862bfff59e01f1c7aaf55ed
Chris Xiong <chirs241097@gmail.com>
2015-10-06 21:13:57 +0800

    Update licenses. SMELT now uses LGPLv3. BLR3 still goes under GPLv3. SMELT will be uploaded to a new repo (BearKidsTeam/SMELT).

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * Bullet Lab Remix 3, main.cpp, main function entry point and SMELT initialization
+ *
+ * This file is part of BLR3.
+ *
+ * BLR3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BLR3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 #include "smelt.hpp"
 #include "master/master.hpp"
 #include "master/resources.hpp"
